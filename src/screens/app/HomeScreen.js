6cd33e986cf0a46c8acd8ab4ff7c13b8e89d4e58
cheolgyu <rb7480@naver.com>
2019-11-03 04:19:25 +0900

    env랑 웹뷰

--- src/screens/app/HomeScreen.js
+++ src/screens/app/HomeScreen.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import env from 'react-native-config'
+import { WebView } from 'react-native-webview';
 
 class HomeScreen extends React.Component {
+
+    componentDidMount() {
+        let res = this._init();
+    }
+
+    async _init() {
+        const URL = env.API_URL + "shops";
+        console.log("==============================");
+        console.log(URL);
+        try {
+            let response = await fetch(URL, {
+                method: 'GET'
+            });
+            let responseJson = await response.json();
+            console.log("==============================");
+            console.log(responseJson);
+            return responseJson;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>HomeScreen Screen</Text>
-            </View>
+            <WebView
+                source={{ uri: 'https://github.com/facebook/react-native' }}
+                style={{ marginTop: 20 }}
+            />
         );
     }
 }
